6a1c88a40a375a7516aa064a291f40074cded72a
Anna Lawley and Polina Nenchev <alawley1@gmail.com>
2016-02-16 10:36:51 -0800

    add initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class Tamagotchi {
+  public static void main(String[] args) {
+    
+  }
+}
